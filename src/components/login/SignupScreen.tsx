69a010d6009d01f5e4b06a54b53fcaef168c0bab
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-03-25 17:05:40 +0100

    Another try, different version

--- src/components/login/SignupScreen.tsx
+++ src/components/login/SignupScreen.tsx
@@ -2,16 +2,23 @@ import RegisterForm from "./SignupForm";
 import { SecurityProvider } from "../../contexts/SecurityContext";
 import Layout from "../common/layout/Layout";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Content = (props: { type: "signup" | "login" }) => {
   const { requestId } = useParams();
+  const [type, setType] = useState<"signup" | "login">();
+
+  useEffect(() => {
+    setType(props.type);
+  }, [setType, props.type]);
+
   return (
     <>
       <Layout
         header="LamaLab"
         content={
           <section className="flex justify-center">
-            {requestId ? (
+            {type === "signup" ? (
               <RegisterForm type={props.type} requestId={requestId} />
             ) : (
               <RegisterForm type={props.type} />
